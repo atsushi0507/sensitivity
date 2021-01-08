a3c4b5cae8dfc5f97360de4a7165eed7a7532bf7
amizukam <atsushi.mizukami@cern.ch>
2021-01-08 10:09:50 +0900

    Add weight

--- cutflow.py
+++ cutflow.py
@@ -1,48 +1,110 @@
+import os
+import math
 import ROOT as r
-import os, glob, sys
-sys.path.append("/Users/amizukam/DVJets/atlasstyle/")
+from getParams import *
+import json
+from array import array
+
+## AM's own version
+import sys
+#sys.path.append("/Users/amizukam/DVJets/atlasstyle")
+sys.path.append("/Users/atsushi/mvstudy_dv")
 from AtlasStyle import *
 from AtlasLabel import *
-from getParams import *
-
 SetAtlasStyle()
+label = "Work in Progress"
+
 r.gROOT.SetBatch()
 
-filepath = "/Volumes/LaCie/DVJets/mc/signal/mc16d/"
-inputFile = "mc16d.txt"
+binLabels = ["Initial", "pass trig", "pass DRAW", "pass Baseline", "DV < 300 mm", "DV > 4 mm from PV", "chisq < 5", "material veto", "strict material veto", "DV nTrk >= 5", "mDV > 10 GeV"]
+cutflow = r.TH1D("cutflow", "cutflow", 11, 0, 11)
+h_div = r.TH1D("h_div", "h_div", 11, 0, 11)
+for i, name in enumerate(binLabels):
+    cutflow.GetXaxis().SetBinLabel(i+1, name)
+    h_div.GetXaxis().SetBinLabel(i+1, name)
+
+#filepath = "/Volumes/LaCie/DVJets/mc/signal/mc16d/"
+filepath = "/Users/atsushi/DVJets/"
+inputFile = "test.txt"
 
 infiles = open(inputFile)
 files = infiles.readlines()
 
+lumiInPb = 136000.0
+xsFile = open("data/13TeV_gluglu_NNLO_NNLL.json")
+xSecs = json.load(xsFile)
+
 c = r.TCanvas("c", "c", 800, 600)
+
+def getNumber(cut):
+    tree.Draw(">>elist", cut)
+    elist = r.gDirectory.Get("elist")
+    nPassed = elist.GetN()
+    return nPassed
 
 for filename in files:
     filename = filename.replace("\n", "")
     infile = r.TFile(filepath + filename, "READ")
     tree = infile.Get("trees_SRDV_")
-    
+
     suffix = filename.split("/")[-1]
     print(suffix)
     dsid = int(suffix.split(".")[2])
     gluinoMass, chi0Mass, tau = getParameters(dsid)
     outputName = "cutflow_" + str(gluinoMass) + "_" + str(chi0Mass) + "_" + tau
 
-    outputDir = "output"
+    outputDir = "output2"
     if (not os.path.isdir(outputDir)):
         os.makedirs(outputDir)
+
     outputFile = r.TFile(outputDir + "/" + outputName + ".root", "RECREATE")
+    """
+    cut = ""
+    initial = getNumber(cut)
+    cut += "DRAW_pass_triggerFlags"
+    passTrig = getNumber(cut)
+    cut += "&&DRAW_pass_DVJETS"
+    passDVJETS = getNumber(cut)
+    cut += "&&BaselineSel_pass"
+    passBaseline = getNumber(cut)
+    cut += "&&DV_passFiducialCut"
+    passFiducial = getNumber(cut)
+    cut += "&&DV_passDistCut"
+    passDistCut = getNumber(cut)
+    cut += "&&DV_passChiSqCut"
+    passChiSqCut = getNumber(cut)
+    cut += "&&DV_passMaterialVeto"
+    passMaterial = getNumber(cut)
+    cut += "&&DV_passMaterialVeto_strict"
+    passMaterial_strict = getNumber(cut)
+    cut += "&&DV_passNTrkCut"
+    passNTrk = getNumber(cut)
+    cut += "&&DV_passMassCut"
+    passMass = getNumber(cut)
 
-    label = "#font[52]{m_{#tilde{g}}} = " + str(gluinoMass) + " GeV, #font[52]{m}_{#font[152]{#tilde{c}_{1}^{0}}} = " + str(chi0Mass) + ", #font[152]{t} = " + tau.replace("p", "0.")
+    xSec = xSecs[str(gluinoMass)]["xsInPb"]
+    mcWeight = 0
 
-    nCuts = 11
-    binLabels = ["Initial", "pass trig", "pass DRAW", "pass Baseline", "DV < 300 mm", "DV > 4 mm from PV", "chisq < 5", "material veto", "strict material veto", "DV nTrk >= 5", "DV mass > 10 GeV"]
-    cutflow = r.TH1D("cuflow", label, nCuts, 0, nCuts)
-    h_div = r.TH1D("h_div", label, nCuts, 0, nCuts)
-    for i, name in enumerate(binLabels):
-        cutflow.GetXaxis().SetBinLabel(i+1, name)
-        h_div.GetXaxis().SetBinLabel(i+1, name)
+    print(initial, passTrig, passDVJETS, passBaseline, passFiducial, passDistCut, passChiSqCut, passMaterial, passMaterial_strict, passNTrk, passMass)
 
+    cutflow.SetBinContent(1, initial)
+    cutflow.SetBinContent(2, passTrig)
+    cutflow.SetBinContent(3, passDVJETS)
+    cutflow.SetBinContent(4, passBaseline)
+    cutflow.SetBinContent(5, passFiducial)
+    cutflow.SetBinContent(6, passDistCut)
+    cutflow.SetBinContent(7, passChiSqCut)
+    cutflow.SetBinContent(8, passMaterial)
+    cutflow.SetBinContent(9, passMaterial_strict)
+    cutflow.SetBinContent(10, passNTrk)
+    cutflow.SetBinContent(11, passMass)
+    
+    cutflow.Draw("hist text")
+    c.Print("Cutflow2/{}.pdf".format(outputName))
+    """
     entries = tree.GetEntries()
+    sumOfWeights = infile.Get("MetaData_EventCount").GetBinContent(3)
+    xSec = xSecs[str(gluinoMass)]["xsInPb"]
     for entry in range(entries):
         ientry = tree.LoadTree(entry)
         if ientry < 0:
@@ -56,23 +118,24 @@ for filename in files:
         passChiSqCut = False
         passMaterial = False
         passMaterial_strict = False
-        passNTrack   = False
+        passNTrk     = False
         passMassCut  = False
 
-        cutflow.AddBinContent(1)
-        for i in range(nCuts):
-            h_div.AddBinContent(i+1)
-
+        mcWeight = tree.mcEventWeight
+        weight = (mcWeight/sumOfWeights) * xSec * lumiInPb
+        
+        cutflow.Fill(0, weight)
         if (not ord(tree.DRAW_pass_triggerFlags)):
             continue
-        cutflow.AddBinContent(2)
+        cutflow.Fill(1, weight)
         if (not ord(tree.DRAW_pass_DVJETS)):
             continue
-        cutflow.AddBinContent(3)
+        cutflow.Fill(2, weight)
         if (not ord(tree.BaselineSel_pass)):
             continue
-        cutflow.AddBinContent(4)
-
+        cutflow.Fill(3, weight)
+        if (tree.DV_n < 1):
+            continue
         for idv in range(len(tree.DV_m)):
             if (not tree.DV_passFiducialCut[idv]):
                 continue
@@ -97,41 +160,35 @@ for filename in files:
             if (not tree.DV_passNTrkCut[idv]):
                 continue
             else:
-                passNTrack = True
+                passNTrk = True
             if (not tree.DV_passMassCut[idv]):
                 continue
             else:
                 passMassCut = True
-
         if (not passFiducial):
             continue
-        cutflow.AddBinContent(5)
+        cutflow.Fill(4, weight)
         if (not passDistCut):
             continue
-        cutflow.AddBinContent(6)
+        cutflow.Fill(5, weight)
         if (not passChiSqCut):
             continue
-        cutflow.AddBinContent(7)
+        cutflow.Fill(6, weight)
         if (not passMaterial):
             continue
-        cutflow.AddBinContent(8)
+        cutflow.Fill(7, weight)
         if (not passMaterial_strict):
             continue
-        cutflow.AddBinContent(9)
-        if (not passNTrack):
+        cutflow.Fill(8, weight)
+        if (not passNTrk):
             continue
-        cutflow.AddBinContent(10)
+        cutflow.Fill(9, weight)
         if (not passMassCut):
             continue
-        cutflow.AddBinContent(11)
-                                    
+        cutflow.Fill(10, weight)
+        
     cutflow.Draw("hist text")
-    c.Print("Cutflow/{}.pdf".format(outputName))
-    cutflow_norm = cutflow.Clone("cutflow_norm")
-    cutflow_norm.GetYaxis().SetRangeUser(0, 1.05)
-    cutflow_norm.Divide(h_div)
-    cutflow_norm.Draw("hist text")
-    c.Print("Cutflow/{}_normalized.pdf".format(outputName))
-
+    c.Print("Cutflow2/{}.pdf".format(outputName))
+    
     outputFile.Write()
     outputFile.Close()
