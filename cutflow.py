ffe5693b1ace4d11ff5b0faad37a893a39d73527
amizukam <a.mizukami.0507@gmail.com>
2021-01-12 21:27:34 +0900

    Applied event weight

--- cutflow.py
+++ cutflow.py
@@ -1,14 +1,11 @@
 import os
-import math
 import ROOT as r
 from getParams import *
 import json
 from array import array
 
-## AM's own version
 import sys
 sys.path.append("/Users/amizukam/DVJets/atlasstyle")
-#sys.path.append("/Users/atsushi/mvstudy_dv")
 from AtlasStyle import *
 from AtlasLabel import *
 SetAtlasStyle()
@@ -17,30 +14,30 @@ label = "Work in Progress"
 r.gROOT.SetBatch()
 
 binLabels = ["Initial", "pass trig", "pass DRAW", "pass Baseline", "DV < 300 mm", "DV > 4 mm from PV", "chisq < 5", "material veto", "strict material veto", "DV nTrk >= 5", "mDV > 10 GeV"]
-cutflow = r.TH1D("cutflow", "cutflow", 11, 0, 11)
-h_div = r.TH1D("h_div", "h_div", 11, 0, 11)
-cutflow.Sumw2()
-h_div.Sumw2()
+
+nBins = len(binLabels)
+cutflow = r.TH1D("cutflow", "cutflow", nBins, 0, nBins)
 for i, name in enumerate(binLabels):
     cutflow.GetXaxis().SetBinLabel(i+1, name)
-    h_div.GetXaxis().SetBinLabel(i+1, name)
 
 filepath = "/Volumes/LaCie/DVJets/mc/signal/mc16d/"
-#filepath = "/Users/atsushi/DVJets/"
 inputFile = "data/mc16d.txt"
-#inputFile = "test.txt"
 
 infiles = open(inputFile)
 files = infiles.readlines()
 
+"""
 lumiInPb = 136.0 * 0.001
 xsFile = open("data/13TeV_gluglu_NNLO_NNLL.json")
-xSecs = json.load(xsFile)
+xSecs =json.load(xsFile)
+"""
+canvases = {}
+histos = {}
+
+r.gStyle.SetPaintTextFormat(".3f")
 
 outputFile = open("data/count.txt", "w")
-
-c = r.TCanvas("c", "c", 800, 600)
-for filename in files:        
+for filename in files:
     filename = filename.replace("\n", "")
     infile = r.TFile(filepath + filename, "READ")
     tree = infile.Get("trees_SRDV_")
@@ -49,11 +46,23 @@ for filename in files:
     print(suffix)
     dsid = int(suffix.split(".")[2])
     gluinoMass, chi0Mass, tau = getParameters(dsid)
-    outputName = "cutflow_" + str(gluinoMass) + "_" + str(chi0Mass) + "_" + tau
+    name = "cutflow_" + str(gluinoMass) + "_" + str(chi0Mass) + "_" + tau
 
+    canvases[dsid] = r.TCanvas(name, name, 800, 600)
+    histos[dsid] = r.TH1D(name, name, nBins, 0, nBins)
+    histos[dsid].SetMinimum(0.)
+    for i, binLabel in enumerate(binLabels):
+        histos[dsid].GetXaxis().SetBinLabel(i+1, binLabel)
+    
     entries = tree.GetEntries()
-    sumOfWeights = infile.Get("MetaData_EventCount").GetBinContent(3)
+    sumW = infile.Get("MetaData_EventCount").GetBinContent(3)
+    """
     xSec = xSecs[str(gluinoMass)]["xsInPb"]
+
+    totalMCEvent = entries
+    lumiMC = totalMCEvent / xSec
+    print(lumiMC)
+    """
     for entry in range(entries):
         ientry = tree.LoadTree(entry)
         if ientry < 0:
@@ -63,30 +72,27 @@ for filename in files:
             continue
 
         passFiducial = False
-        passDistCut  = False
-        passChiSqCut = False
+        passDist     = False
+        passChiSq    = False
         passMaterial = False
         passMaterial_strict = False
         passNTrk     = False
-        passMassCut  = False
+        passMass     = False
 
         mcWeight = tree.mcEventWeight
-        #weight = (mcWeight/sumOfWeights) * xSec * lumiInPb
-        weight = mcWeight * xSec * lumiInPb
-        cutflow.Fill(0, weight)
-        for i in range(len(binLabels)):
-            h_div.Fill(i, weight)
+        weight = mcWeight/sumW
+
+        histos[dsid].AddBinContent(1, weight)
         if (not ord(tree.DRAW_pass_triggerFlags)):
             continue
-        cutflow.Fill(1, weight)
+        histos[dsid].AddBinContent(2, weight)
         if (not ord(tree.DRAW_pass_DVJETS)):
             continue
-        cutflow.Fill(2, weight)
+        histos[dsid].AddBinContent(3, weight)
         if (not ord(tree.BaselineSel_pass)):
             continue
-        cutflow.Fill(3, weight)
-        if (tree.DV_n < 1):
-            continue
+        histos[dsid].AddBinContent(4, weight)
+
         for idv in range(len(tree.DV_m)):
             if (not tree.DV_passFiducialCut[idv]):
                 continue
@@ -95,11 +101,11 @@ for filename in files:
             if (not tree.DV_passDistCut[idv]):
                 continue
             else:
-                passDistCut = True
+                passDist = True
             if (not tree.DV_passChiSqCut[idv]):
                 continue
             else:
-                passChiSqCut = True
+                passChiSq = True
             if (not tree.DV_passMaterialVeto[idv]):
                 continue
             else:
@@ -115,40 +121,33 @@ for filename in files:
             if (not tree.DV_passMassCut[idv]):
                 continue
             else:
-                passMassCut = True
+                passMass = True
 
         if (not passFiducial):
             continue
-        cutflow.Fill(4, weight)
-        if (not passDistCut):
+        histos[dsid].AddBinContent(5, weight)
+        if (not passDist):
             continue
-        cutflow.Fill(5, weight)
-        if (not passChiSqCut):
+        histos[dsid].AddBinContent(6, weight)
+        if (not passChiSq):
             continue
-        cutflow.Fill(6, weight)
+        histos[dsid].AddBinContent(7, weight)
         if (not passMaterial):
             continue
-        cutflow.Fill(7, weight)
+        histos[dsid].AddBinContent(8, weight)
         if (not passMaterial_strict):
             continue
-        cutflow.Fill(8, weight)
+        histos[dsid].AddBinContent(9, weight)
         if (not passNTrk):
             continue
-        cutflow.Fill(9, weight)
-        if (not passMassCut):
+        histos[dsid].AddBinContent(10, weight)
+        if (not passMass):
             continue
-        cutflow.Fill(10, weight)
-        
-    cutflow.Draw("hist text")
-    c.Print("Cutflow/{}.pdf".format(outputName))
+        histos[dsid].AddBinContent(11, weight)
 
-    cutflow_norm = cutflow.Clone("cutflow_norm")
-    cutflow_norm.GetYaxis().SetRangeUser(0, 1.05)
-    cutflow_norm.Divide(h_div)
-    cutflow_norm.Draw("hist text")
-    c.Print("Cutflow/{}_normalized.pdf".format(outputName))
+    histos[dsid].Draw("hist text")
+    canvases[dsid].Print("Cutflow/{}.pdf".format(name))
 
-    N = cutflow.GetBinContent(11)
-    initial = cutflow.GetBinContent(1)
-    outputFile.write("{} {} {} {} {} {} {}%\n".format(dsid, str(gluinoMass), str(chi0Mass), str(tau), initial, N, (N/initial)*100))
-    print("{} {} {} {} {} {} {}%".format(dsid, str(gluinoMass), str(chi0Mass), str(tau), initial, N, (N/initial)*100))
+    initial = histos[dsid].GetBinContent(1)
+    passed = histos[dsid].GetBinContent(11)
+    outputFile.write("{}\t{}\t{}\n".format(dsid, initial, passed))
